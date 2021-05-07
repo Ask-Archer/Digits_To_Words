55544483a486c4f08be660be72a27eaa71be90b5
Ask-Archer <75997124+Ask-Archer@users.noreply.github.com>
2021-05-07 17:03:56 -0500

    Update digits_to_words.py
    Added an exception for strings

--- digits_to_words.py
+++ digits_to_words.py
@@ -2,19 +2,23 @@ ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
 
 tens = ['ten','twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
-position = input("Enter a number from 01-99 using digits: ")
+def convert():
+  for item in range(1):
+      try:
+        position = input("Enter a number from 01-99 using digits: ")
+        position_int = int(position)
+        first_digit = int(position[0])
+        second_digit = int(position[1])
 
-position_int = int(position)
+        if position_int > 99:
+            print("Too high.")
+        elif first_digit == 0 or first_digit == 1:
+            print(ones[position_int - 1])
+        elif second_digit == 0:
+            print(tens[first_digit - 1])
+        else:
+            print((tens[first_digit - 1]) + "-" + (ones[second_digit - 1]))
+      except ValueError:
+          print("Please use a valid number. ")
 
-first_digit = int(position[0])
-second_digit = int(position[1])
-
-for item in range(1):
-  if position_int > 99:
-    print("Too high.")
-  elif first_digit == 0 or first_digit == 1:
-    print(ones[position_int -1])
-  elif second_digit == 0:
-    print(tens[first_digit - 1])
-  else:
-    print((tens[first_digit -1]) + "-" +(ones[second_digit -1]))
+convert()
